92afb4669888d59e1a979f823b45c42fc53e74bc
Sebas Echazú <sebastianechazu@outlook.com>
2024-09-10 12:41:41 -0300

    fix tokens

--- src/services/jwt.js
+++ src/services/jwt.js
@@ -3,6 +3,7 @@
 import jwt from 'jwt-simple';
 import moment from 'moment';
 import { config } from 'dotenv';
+import logger from '../utils/logger.js';
 
 config();
 
@@ -21,20 +22,20 @@ export const createToken = (user) => {
 };
 
 export const authenticateUser = async (req, res, next) => {
-    try {
-        
-        if (!req.headers.authorization) {
 
-            const error = new Error('The request does not have the authentication header');
-            error.status = 401;
-            throw error;
+    try {
+
+        if (!req.headers.authorization) {
+            logger.error('The request does not have the authentication header');
+            return res.status(401).send({ message: 'The request does not have the authentication header' });
+
         }
 
         const token = req.headers.authorization.replace(/Bearer /, '').trim();
-
         const payload = jwt.decode(token, secret);
 
-        if (payload.exp <= moment().unix()) {
+        if (payload.exp <= moment().unix()){    
+            logger.error('The token has expired');
             return res.status(401).send({ message: 'The token has expired' });
         }
 
@@ -42,7 +43,7 @@ export const authenticateUser = async (req, res, next) => {
         next();
 
     } catch (error) {
-        
+        logger.error('The token is invalid')
         return res.status(404).send({ message: 'The token is invalid' });
     }
 };
